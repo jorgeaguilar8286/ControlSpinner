c056d3877c3d1df58213dc0b45548d5b7154615f
Jorge Aguilar <2014djjav@gmail.com>
2020-11-27 13:44:06 -0300

    modificacion colorspinner

--- app/src/main/java/com/example/controlspinner/MainActivity.java
+++ app/src/main/java/com/example/controlspinner/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         spinner1 = (Spinner)findViewById(R.id.spinner);
 
         String [] opciones = {"sumar","restar","multiplicar","dividir"};
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>( this, android.R.layout.simple_spinner_item, opciones);
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>( this, R.layout.spinner_item_djjav, opciones);
         spinner1.setAdapter(adapter);
     }
 
